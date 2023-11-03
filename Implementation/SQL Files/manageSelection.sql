49da59fb2e1c89c695f60c12cf0a6550fa31ca99
Michael Liao <michael.liao@centre.edu>
2023-11-03 21:42:39 +0000

    outlined a management sql

--- Implementation/SQL Files/manageSelection.sql
+++ Implementation/SQL Files/manageSelection.sql
@@ -1,5 +1,5 @@
 DELIMITER //
-CREATE OR REPLACE PROCEDURE add_print_material(title VARCHAR(256), date_received DATE, date_created DATE, )
+CREATE OR REPLACE PROCEDURE add_print_material(title VARCHAR(256), date_received DATE, date_created DATE, is_pending BOOLEAN, price DECIMAL(10,2), num_pages INT, print_type VARCHAR(256))
  BEGIN
  START TRANSACTION;
        
