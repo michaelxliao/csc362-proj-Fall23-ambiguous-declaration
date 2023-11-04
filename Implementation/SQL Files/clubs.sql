06a4f0f3710f6bd9bb84199c4bc543b2cce6d439
Michael Liao <michael.liao@centre.edu>
2023-11-04 02:26:29 +0000

    fixed clubs

--- Implementation/SQL Files/clubs.sql
+++ Implementation/SQL Files/clubs.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE clubs(
     PRIMARY KEY(club_id),
-    club_id             INT AUTO_INCREMENT,
-    club_name           VARCHAR(64),
-    club_description    VARCHAR(250),
-    club_isActive       BOOLEAN DEFUALT TRUE
+    club_id             INT             AUTO_INCREMENT,
+    club_name           VARCHAR(256),
+    club_description    VARCHAR(1024),
+    club_is_active      BOOLEAN         DEFAULT TRUE
 );
