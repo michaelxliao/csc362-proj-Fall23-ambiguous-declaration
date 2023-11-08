6cdb96de3edfa82a7ce59c7515d385685bba7e90
Rashawn <rashawn.butler03925@gmail.com>
2023-11-08 01:08:51 +0000

    Fixing club_id to club_name

--- Implementation/SQL Files/clubReservations.sql
+++ Implementation/SQL Files/clubReservations.sql
@@ -4,9 +4,9 @@ CREATE OR REPLACE TABLE club_reservations (
     PRIMARY KEY (reservation_id),
     FOREIGN KEY (reservation_id) 
         REFERENCES space_reservations(reservation_id),
-    FOREIGN KEY (club_id)
-        REFERENCES clubs(club_id),
+    FOREIGN KEY (club_name)
+        REFERENCES clubs(club_name),
 
     reservation_id      INT,
-    club_id             INT
+    club_name             INT
 );
