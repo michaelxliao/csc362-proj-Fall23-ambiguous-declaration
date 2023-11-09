a88b9d9160829d8904e26feefcb4bc4e460bb8c1
Rashawn <rashawn.butler03925@gmail.com>
2023-11-09 21:21:05 +0000

    Fixing File

--- Implementation/SQL Files/manageClubs.sql
+++ Implementation/SQL Files/manageClubs.sql
@@ -1,8 +1,9 @@
 DELIMITER //
-CREATE OR REPLACE PROCEDURE add_Club(club_name VARCHAR(256), club_description)
+CREATE OR REPLACE PROCEDURE add_Club(new_club_name VARCHAR(256), new_club_description)
  BEGIN
  START TRANSACTION;
-       
+       INSERT INTO clubs( club_name, club_description)
+       VALUES(new_club_name, new_club_description);
 COMMIT;
    END
 //
--- Implementation/SQL Files/managePatrons.sql
+++ Implementation/SQL Files/managePatrons.sql
@@ -1,10 +1,22 @@
 DELIMITER //
-CREATE OR REPLACE PROCEDURE add_patron(Patron_First_Name VARCHAR(256), Patron_Last_Name VARCHAR(256), Patron_Email VARCHAR(256), Patron_Phone VARCHAR(64))
+CREATE OR REPLACE PROCEDURE add_patron(new_Patron_First_Name VARCHAR(256), new_Patron_Last_Name VARCHAR(256), new_Patron_Email VARCHAR(256), new_Patron_Phone VARCHAR(64))
  BEGIN
  START TRANSACTION;
-       
+       INSERT INTO patrons
+       VALUES(new_Patron_First_Name, new_Patron_Last_Name, new_Patron_Email, new_Patron_Phone)
 COMMIT;
    END
 //
 DELIMITER ;
 
+DELIMITER //
+CREATE OR REPLACE PROCEDURE del_patron(del_Patron_ID)
+ BEGIN
+ START TRANSACTION;
+   DELETE FROM patrons
+   WHERE Patron_ID=del_Patron_ID;
+
+COMMIT;
+   END
+//
+DELIMITER ;
