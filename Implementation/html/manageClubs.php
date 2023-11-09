1272ecc958c531fcec9e95a5dd437f71af32d745
Rashawn <rashawn.butler03925@gmail.com>
2023-11-09 22:04:06 +0000

    Udpating page

--- Implementation/html/manageClubs.php
+++ Implementation/html/manageClubs.php
@@ -1,10 +1,31 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$config = parse_ini_file('/home/rashawn_butler/mysql.ini');
+$dbname = 'therpston';
+print_r($config);
+$conn = new mysqli(
+    $config['lib_host'],
+    $config['lib_user'],
+    $config['lib_password'],
+    $config['lib_database']
+);
+
+if ($conn->connect_errno) {
+    echo "Error: Failed to make a MySQL connection, here is why: ". "<br>";
+    echo "Errno: " . $conn->connect_errno . "\n";
+    echo "Error: " . $conn->connect_error . "\n";
+exit; // Quit this PHP script if the connection fails.
+};
+
 ?>
 <html>
     <head>
         <title>Managing Clubs Page</title>
         <body>
-
+            <p>Hello World. This will be where clubs are managed</p>
         </body>
     </head>
 </html>
