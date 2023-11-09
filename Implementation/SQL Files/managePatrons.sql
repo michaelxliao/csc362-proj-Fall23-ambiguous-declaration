35851cba8ddb5586850ad9f5039b761de54e7c59
Rashawn <rashawn.butler03925@gmail.com>
2023-11-09 21:36:27 +0000

    Updating file

--- Implementation/SQL Files/managePatrons.sql
+++ Implementation/SQL Files/managePatrons.sql
@@ -10,6 +10,22 @@ COMMIT;
 DELIMITER ;
 
 DELIMITER //
+CREATE OR REPLACE PROCEDURE update_patron(id INT, new_patron_first_name VARCHAR(256), new_patron_last_name VARCHAR(256), new_ptron_email VARCHAR(256), new_patron_phone CHAR(14))
+ BEGIN
+ START TRANSACTION;
+       UPDATE patrons
+       SET patron_first_name=new_patron_first_name,
+           patron_last_name=new_patron_last_namem,
+           patron_email=new_patron_email,
+           patron_phone=new_patron_phone
+           WHERE patron_id=id;
+ COMMIT;
+   END
+
+//
+DELIMITER ;
+
+DELIMITER //
 CREATE OR REPLACE PROCEDURE del_patron(del_Patron_ID)
  BEGIN
  START TRANSACTION;
