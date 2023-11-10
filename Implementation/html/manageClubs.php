12ff1eb0f2e59eeee2f11d66286d062677066cdd
Michael Liao <michael.liao@centre.edu>
2023-11-10 20:32:38 +0000

    refactored setup into a little function

--- Implementation/html/manageClubs.php
+++ Implementation/html/manageClubs.php
@@ -1,24 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$config = parse_ini_file('/home/stard/mysql.ini');
-$dbname = 'therpston';
-print_r($config);
-$conn = new mysqli(
-    $config['lib_host'],
-    $config['lib_user'],
-    $config['lib_password'],
-    $config['lib_database']
-);
-
-if ($conn->connect_errno) {
-    echo "Error: Failed to make a MySQL connection, here is why: ". "<br>";
-    echo "Errno: " . $conn->connect_errno . "\n";
-    echo "Error: " . $conn->connect_error . "\n";
-exit; // Quit this PHP script if the connection fails.
-};
+require 'setup.php';
+$conn = setup();
 
 ?>
 <html>
---
+++ Implementation/html/setup.php
@@ -0,0 +1,41 @@
+<?php
+function doErrorReporting() {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
+function connectToDB() {
+    $config = parse_ini_file('/home/stard/mysql.ini');
+    if (!$config) { // if this fails, try another directory (NOTE: WILL PRODUCE WARNINGS! DISABLE WHEN DONE)
+        $config = parse_ini_file('/home/michael/mysql.ini');
+        if (!$config) {
+            $config = parse_ini_file('/home/<RASHAWN USERNAME HERE>/mysql.ini');
+        }
+    }
+
+    $conn = new mysqli(
+                $config['lib_host'],
+                $config['lib_user'],
+                $config['lib_password'],
+                $config['lib_database']);
+    if ($conn->connect_errno) {
+            echo "Error: Failed to make a MySQL connection, here is why: ". "<br>";
+            echo "Errno: " . $conn->connect_errno . "\n";
+            echo "Error: " . $conn->connect_error . "\n";
+            exit; // Quit this PHP script if the connection fails.
+        } else {
+            echo "Connection established." . "<br>";
+        }
+
+    return $conn;
+}
+
+function setup($enable_errors = true) { // can turn off enable errors by passing false bool
+    if ($enable_errors) {
+        doErrorReporting();
+    }
+    return connectToDB();
+}
+
+?>
