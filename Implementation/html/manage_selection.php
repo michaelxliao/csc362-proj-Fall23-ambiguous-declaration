f562aa6d33979a6a371f91c6331cdb79a3a7c2a7
Michael Liao <michael.liao@centre.edu>
2023-11-17 21:31:15 +0000

    more robust selection

--- Implementation/html/manage_selection.php
+++ Implementation/html/manage_selection.php
@@ -19,27 +19,23 @@ $PRINT = 'Print';
 $MULTIMEDIA = 'Multimedia';
 
 if (isset($_GET["Filter"])) {
-    echo $_GET["media-type"];
+    // echo $_GET["media-type"];
     if ($_GET["media-type"] == $ALL_MEDIA) {
         $sql_query = 'SELECT * FROM pretty_active_selection_librarians'; // this by default; can be overridden by filtering
         $result = $conn->query($sql_query);
     }
     if ($_GET["media-type"] == $PRINT) {
         $sql_query = 'SELECT * FROM pretty_active_selection_librarians
-                                    LEFT OUTER JOIN print_materials ON pretty_active_selection_librarians.`ID` = print_materials.material_id
                        WHERE pretty_active_selection_librarians.`ID` IN (SELECT material_id FROM print_materials)';
         $result = $conn->query($sql_query);
     } elseif ($_GET["media-type"] == $MULTIMEDIA) {
         $sql_query = 'SELECT * FROM pretty_active_selection_librarians
-                                    LEFT OUTER JOIN multimedia ON pretty_active_selection_librarians.`ID` = multimedia.material_id
-                      WHERE pretty_active_selection_librarians.`ID` IN (SELECT material_id FROM multimedia)';
+                       WHERE pretty_active_selection_librarians.`ID` IN (SELECT material_id FROM multimedia)';
         $result = $conn->query($sql_query);
     } elseif ($_GET["media-type"] != $ALL_MEDIA) {
         $filter_stmt = $conn->prepare('SELECT * FROM pretty_active_selection_librarians
-                                      LEFT OUTER JOIN print_materials ON pretty_active_selection_librarians.`ID` = print_materials.material_id
-                                      LEFT OUTER JOIN multimedia ON pretty_active_selection_librarians.`ID` = multimedia.material_id
-                                WHERE (multimedia_type = ?) OR (print_type = ?)');
-        $filter_stmt->bind_param('ss', $_GET["media-type"], $_GET["media-type"]);
+                                WHERE `Type` = ?');
+        $filter_stmt->bind_param('s', $_GET["media-type"]);
         if ($filter_stmt->execute()) {
             $result = $filter_stmt->get_result();
         } else {
--- Implementation/html/sql/views.sql
+++ Implementation/html/sql/views.sql
@@ -3,11 +3,11 @@
 CREATE OR REPLACE VIEW pretty_patron_details_librarian
 AS (
 SELECT 
-    patron_id AS "ID", -- assuming this is the same as their library card's
-    (CONCAT(patron_first_name, " ", patron_last_name)) AS "Name",
-    COUNT(material_id) AS "Number of Loans", -- need only current loans?
-    patron_email AS "Email",
-    patron_phone AS "Phone Number"
+    patron_id AS 'ID', -- assuming this is the same as their library card's
+    (CONCAT(patron_first_name, ' ', patron_last_name)) AS 'Name',
+    COUNT(material_id) AS 'Number of Loans', -- need only current loans?
+    patron_email AS 'Email',
+    patron_phone AS 'Phone Number'
 
     FROM patrons
         LEFT OUTER JOIN patron_selection_interactions USING (patron_id)
@@ -17,33 +17,38 @@ SELECT
     GROUP BY (patron_id)
 );
 
-CREATE OR REPLACE VIEW current_loans AS
-SELECT *
-  FROM loans
-       LEFT OUTER JOIN patron_selection_interactions USING(interaction_id)
- WHERE loan_return_date IS NOT NULL;
 
-CREATE OR REPLACE VIEW pretty_active_selection_librarians AS -- needs reformatting lol
+CREATE OR REPLACE VIEW pretty_active_selection_librarians AS
 SELECT material_id AS 'ID',
-       material_title AS "Title",
+       material_title AS 'Title',
        (CASE WHEN (NOT EXISTS (SELECT 1 FROM current_loans WHERE current_loans.material_id = active_selection.material_id))
         THEN 'On Shelf'
         ELSE 'Checked Out'
         END) AS 'Available for Checkout?', -- should be filterable
-       material_date_received AS "Date Selected into Library",
-       material_date_created AS "Date Created",
+       material_date_received AS 'Date Selected into Library', -- prolly leave to drill-down
+       material_date_created AS 'Date Created',
        (CASE WHEN material_is_pending
        THEN 'Not Processed Yet'
        ELSE 'Processed'
-       END) AS "Pending?", -- should be filterable
-       material_price AS "Cost", -- prolly leave to drill-down
-       
+       END) AS 'Pending?', -- should be filterable
+       material_price AS 'Cost', -- prolly leave to drill-down
+       GROUP_CONCAT(CONCAT(creator_first_name, ' ', creator_last_name) SEPARATOR ', ') AS 'Creator(s)', -- add conditional to only show authors/directors
+       GROUP_CONCAT(creator_role SEPARATOR ', ') AS 'Role',
+       (CASE WHEN (multimedia_type IS NULL)
+       THEN print_type
+       ELSE multimedia_type
+       END) AS 'Type',
+       (CASE WHEN (multimedia_type IS NULL)
+       THEN page_count
+       ELSE duration
+       END) AS 'Length'
   FROM active_selection -- change this if want is_active field
-       LEFT OUTER JOIN print_materials ON pretty_active_selection_librarians.`ID` = print_materials.material_id
-       LEFT OUTER JOIN multimedia ON pretty_active_selection_librarians.`ID` = multimedia.material_id
-       LEFT OUTER JOIN selection_creators ON pretty_active_selection_librarians.`ID` = selection_creators.material_id
+       LEFT OUTER JOIN print_materials USING(material_id)
+       LEFT OUTER JOIN multimedia USING(material_id)
+       LEFT OUTER JOIN selection_creators USING(material_id)
        LEFT OUTER JOIN creators USING (creator_id)
-       LEFT OUTER JOIN creator_roles USING (creator_role);
+       LEFT OUTER JOIN creator_roles USING (creator_role)
+ GROUP BY material_id;
 
 --  SELECT * FROM pretty_patron_details_librarian;
 /*
