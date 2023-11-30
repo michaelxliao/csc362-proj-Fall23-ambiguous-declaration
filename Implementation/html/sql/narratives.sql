c6aa4156d01e19e4b79fc9c169e84ad496ded1d0
Rashawn <rashawn.butler03925@gmail.com>
2023-11-30 23:30:37 +0000

    Adding a feature to add narratives

--- Implementation/html/sql/narratives.sql
+++ Implementation/html/sql/narratives.sql
@@ -2,6 +2,6 @@ CREATE OR REPLACE TABLE narratives (
     PRIMARY KEY (narrative_id),
     narrative_id            INT             AUTO_INCREMENT,
     narrative_name          VARCHAR(256)    NOT NULL,
-    narrative_description   VARCHAR(1024)   NOT NULL,
+    narrative_description   VARCHAR(1024),
     narrative_is_active     BOOLEAN         NOT NULL DEFAULT TRUE -- for DENY deletion rule
 );
