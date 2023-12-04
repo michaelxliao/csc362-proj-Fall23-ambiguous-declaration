0902732a3b6816a1fc862e7057f6e5ce35ca0993
piercingecho <piercem4502@gmail.com>
2023-12-04 04:57:20 +0000

    Nullify implemented for multimedia and print types

--- Implementation/html/sql/multimediaTypes.sql
+++ Implementation/html/sql/multimediaTypes.sql
@@ -1,5 +1,4 @@
 CREATE OR REPLACE TABLE multimedia_types (
     PRIMARY KEY (multimedia_type),
-    multimedia_type             VARCHAR(256),
-    multimedia_type_is_active   BOOLEAN         NOT NULL DEFAULT TRUE -- for DENY deletion
+    multimedia_type             VARCHAR(256)
 );
--- Implementation/html/sql/printTypes.sql
+++ Implementation/html/sql/printTypes.sql
@@ -1,5 +1,4 @@
 CREATE OR REPLACE TABLE print_types (
     PRIMARY KEY (print_type),
-    print_type              VARCHAR(256),
-    print_type_is_active    BOOLEAN         NOT NULL DEFAULT TRUE -- for DENY
+    print_type              VARCHAR(256)
 );
